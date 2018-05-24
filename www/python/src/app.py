3412151f5ad732eab7421ef479b90ce411d107bb
Kilian Kluge <kilian.kluge@wikipedia.de>
2018-05-24 12:28:20 +0000

    Simple OAuth web interface

---
+++ www/python/src/app.py
@@ -0,0 +1,64 @@
+import flask
+import os
+import yaml
+import mwoauth
+
+app = flask.Flask(__name__)
+
+__dir__ = os.path.dirname(__file__)
+app.config.update(yaml.safe_load(open(os.path.join(__dir__, 'config.yaml'))))
+
+@app.route("/")
+def index():
+    greeting = app.config["GREETING"]
+    username = flask.session.get("username", None)
+    return flask.render_template("index.html", username=username, greeting=greeting)
+
+@app.route("/login")
+def login():
+    consumer_token = mwoauth.ConsumerToken(
+        app.config["CONSUMER_KEY"], app.config["CONSUMER_SECRET"])
+    try:
+        redirect, request_token = mwoauth.initiate(
+            app.config["OAUTH_MWURI"], consumer_token)
+    except Exception:
+        app.logger.exception("mwoauth.initiate failed")
+        return flask.redirect(flask.url_for("index"))    
+    else:
+        flask.session["request_token"] = dict(zip(request_token._fields, request_token))
+        return flask.redirect(redirect)
+
+@app.route("/oauth-callback")
+def oauth_callback():
+    if "request_token" not in flask.session:
+        flask.flash(u"OAuth callbak failed. Are cookies disabled?")
+        return flask.redirect(flask.url_for("index"))
+
+    consumer_token = mwoauth.ConsumerToken(
+        app.config["CONSUMER_KEY"], app.config["CONSUMER_SECRET"])
+
+    try:
+        access_token = mwoauth.complete(
+            app.config["OAUTH_MWURI"],
+            consumer_token,
+            mwoauth.RequestToken(**flask.session["request_token"]),
+            flask.request.query_string)
+
+        identity = mwoauth.identify(
+            app.config["OAUTH_MWURI"], consumer_token, access_token)
+    except Exception:
+        app.logger.exception("OAuth authentification failed")
+
+    else:
+        flask.session["access_token"] = dict(zip(
+            access_token._fields, access_token))
+        flask.session["username"] = identity["username"]
+
+    return flask.redirect(flask.url_for("index"))
+
+
+@app.route("/logout")
+def logout():
+    flask.session.clear()
+    return flask.redirect(flask.url_for("index"))
+
