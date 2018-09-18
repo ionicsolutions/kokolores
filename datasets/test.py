fc6b17a3614ca71452ef779534a34751a2336184
Kilian Kluge <kilian.kluge@gmail.com>
2018-09-18 07:29:18 +0200

    File logging

--- datasets/test.py
+++ datasets/test.py
@@ -1,5 +1,12 @@
+import logging
+
 from creator import Creator
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+fh = logging.FileHandler("creator.log")
+logger.addHandler(fh)
+logger.info("Starting...")
 c = Creator("dewiki")
 dataset = c.create(start=20160101000000, stop=20161231235959,
                    batch_size=50, fname="test2016.json.bz2")
